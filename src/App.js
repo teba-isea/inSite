e7927c32e92ffa6fd32528677db637b372610ebc
Esteban Isea <iseaesteban@gmail.com>
2020-09-25 00:20:12 +0000

    fixing api request error

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ function App() {
   useEffect(() => {
     const consultApi = async () => {
       const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=b65044def5094b33bc13486d88bf375f`;
-      const req = await fetch(url);
+      const req = await fetch(url,fetch(url, {
+    mode:'no-cors'
+    method: "GET",
+  });
       const res = await req.json();
       setNews(res.articles);
     };
